3951b2c14188f800bacb8b7afcff38c0b6adf0ce
Anton Lofgren <anton.lofgren@gmail.com>
2013-01-22 23:04:02 +0100

    Add message initialisation & destruction

--- lib/libi3ipc.c
+++ lib/libi3ipc.c
@@ -6,6 +6,7 @@
 #include <errno.h>
 #include <unistd.h>
 #include <limits.h>
+#include <stdbool.h>
 
 static int i3ipc_sock;
 /* calls `i3 --get-socketpath` and returns the output
@@ -69,14 +70,70 @@ int i3ipc_sock_connect(const char *sockpath) {
 
 }
 
+/* disconnects from i3 socket or
+ * exits on error
+ */
 void i3ipc_sock_disconnect(void) {
 	if ( close(i3ipc_sock) != 0) {
 		perror("close");
 		exit(1);
 	}
 }
+
+/* returns true if type is a known, valid
+ * i3 IPC message type, returns false otherwise
+ */
+bool i3ipc_valid_type(uint32_t type) {
+	switch(type) {
+		/* switch on known message types,
+		 * fallthrough intentional
+		 * */
+		case I3_IPC_MESSAGE_TYPE_COMMAND:
+		case I3_IPC_MESSAGE_TYPE_GET_WORKSPACES:
+		case I3_IPC_MESSAGE_TYPE_SUBSCRIBE:
+		case I3_IPC_MESSAGE_TYPE_GET_OUTPUTS:
+		case I3_IPC_MESSAGE_TYPE_GET_TREE:
+		case I3_IPC_MESSAGE_TYPE_GET_MARKS:
+		case I3_IPC_MESSAGE_TYPE_GET_BAR_CONFIG:
+		case I3_IPC_MESSAGE_TYPE_GET_VERSION:
+			return true;
+		default: return false;
+	}
+}
+
+
+i3ipc_msg *i3ipc_msg_create(uint32_t type, const char *payload) {
+	i3ipc_msg *m = malloc(sizeof(i3ipc_msg));
+	strcpy(m->magic, I3_IPC_MAGIC);
+	if (!i3ipc_valid_type(type) ) {
+		return NULL;
+	}
+	m->type = type;
+	if (payload) {
+		m->len = strlen(payload);
+		m->payload = strndup(payload, m->len);
+	}
+	else {
+		m->len = 0;
+	}
+	return m;
+}
+
+void i3ipc_msg_destroy(i3ipc_msg *msg) {
+	free(msg->payload);
+	free(msg);
+}
+
+void i3ipc_print_message(i3ipc_msg *msg) {
+	if (!msg)
+		return;
+	printf("I3 IPC Message: %s,%d,%d,%s\n", msg->magic, msg->len, msg->type, msg->payload);
+}
+
 workspace *get_workspaces(void) {
-	workspace *root = malloc(sizeof(workspace*));
+	i3ipc_msg *request = i3ipc_msg_create(I3_IPC_MESSAGE_TYPE_GET_WORKSPACES, NULL);
+	i3ipc_print_message(request);
+	workspace *root = malloc(sizeof(workspace));
 	root->next = NULL;
 	root->name = malloc(128);
 	sprintf(root->name, "temp workspace name");
--- lib/libi3ipc.h
+++ lib/libi3ipc.h
@@ -1,3 +1,12 @@
+#include "i3/ipc.h"
+#include <stdint.h>
+#define MAGIC_LEN (sizeof(I3_IPC_MAGIC)/sizeof(char))
+typedef struct {
+	char magic[MAGIC_LEN];
+	uint32_t len;
+	uint32_t type;
+	char *payload;
+} i3ipc_msg;
 
 int i3ipc_sock_connect(const char *sockpath);
 void i3ipc_sock_disconnect(void);
